66b996c5fc4d0258937641e961617f49d53606f8
nathanjh-28 <nathan@nathanjharris.com>
2021-02-06 13:03:15 -0700

    task component created with delete and reminder functionality

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Header from './components/Header'
 import Tasks from './components/Tasks'
 import { useState } from 'react'
 
+
+
 function App() {
   const [tasks, setTasks] = useState([    {
     id: 0,
@@ -29,14 +31,26 @@ function App() {
     }
 
 ])
+// Delete Task
+const deleteTask = (id) => {
+  setTasks(tasks.filter((task)=> task.id !== id))
+}
+
+// Toggle Reminder
+const toggleReminder = (id) => {
+  setTasks(tasks.map((task)=>task.id === id ? { ...task,reminder: !task.reminder } : task))
+}
+
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={ tasks } />
+      {tasks.length>0 ? <Tasks tasks={ tasks } onDelete={ deleteTask } onToggle={toggleReminder}/> : ('No Tasks to Show')}
 
     </div>
   )
 }
+
+
 
 export default App;
 
